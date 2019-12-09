7c40c7fa0b70bde178a9f5c5743d30b3834e8efb
denisivanov91 <kosmos212002@gmail.com>
2019-12-09 18:01:56 +0200

    my first commit

---
+++ less1/mian.js
@@ -0,0 +1,9 @@
+var letters = []
+var str = "Backend As A Service"
+var arr = str.split(' ')
+letters = arr
+
+for (var i = 0; i < arr.length; i++)
+{
+    console.log(letters[i][0])
+}
---
+++ less2/main.js
@@ -0,0 +1,5 @@
+var num = [254, 115, 78, 25, 91, 45, 37]
+for (var i = 0; i < num.length; i++)
+{
+	num[i] > 50 && console.log(num[i])
+}
