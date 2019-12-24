850eae91d907a4e3cecff73bc28f26b7e74976de
denisivanov91 <kosmos212002@gmail.com>
2019-12-24 02:29:04 +0200

    My New Commit

---
+++ less04/main.js
@@ -0,0 +1,17 @@
+var ladysBag = {
+	phone: 1,
+	mirror: 2,
+	charging: 3,
+	pomade: 5
+}
+
+ladysBag.getDelete = function(){
+	delete ladysBag.phone
+}
+ladysBag.getAdd = function(){
+	ladysBag ["headphones"] = 1
+}
+
+//Надеюсь правильно сделал
+//Я не совсем понял задание
+
